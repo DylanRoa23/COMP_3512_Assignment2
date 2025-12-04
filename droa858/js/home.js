955cc1b058f474fe38be6168b2358d7314b9cea5
DylanRoa23 <dylanroa23@gmail.com>
2025-12-04 00:20:43 -0700

    small slideshow created, dimensons will be resized later

--- droa858/js/home.js
+++ droa858/js/home.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    
+    let slideIndex = 0;
+    showSlides();
 
+    function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("banner");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+    }
+
+    //code taken from https://www.w3schools.com/howto/howto_js_slideshow.asp
 });
