eb8ea2e942de94d65e36307136ccfbaef9e87ffb
Existenial <150177160+Existenial@users.noreply.github.com>
2025-12-04 22:11:22 -0700

    Added comments and reorganized code.

--- droa858/js/browse.js
+++ droa858/js/browse.js
@@ -6,39 +6,39 @@ document.addEventListener("DOMContentLoaded", () => {
     const browse = document.querySelector(".browse");
     const home = document.querySelector(".home");
     const colorsDiv = document.querySelector("#color");
-
     const colorURL = "json/colors.json";
 
-    // Default state
-    home.classList.remove("hidden");
-    browse.classList.add("hidden");
-
-    // Toggle pages
+    // Navigate pages
     browseBtn.addEventListener("click", (e) => {
         e.preventDefault();
         home.classList.add("hidden");
         browse.classList.remove("hidden");
     });
-
     homeBtn.addEventListener("click", (e) => {
         e.preventDefault();
         browse.classList.add("hidden");
         home.classList.remove("hidden");
     });
 
-    //Fetch colors.json which has basic colors plus equivalents 
-    //So yellow will show gold colored clothes and black will show charcoal
+    // Fetch colors.json which has basic colors plus equivalents 
+    // So yellow will show gold colored clothes and black will show charcoal
     fetch(colorURL)
         .then(response => response.json())
         .then(colorData => {
+
+            // Initialize
             const colorHexMap = colorData.hex;
 
             // Only use the grouped color names from hex map
             const groupedColors = Object.keys(colorHexMap).sort(); // sorted alphabetically
-
+            
             // Makes a checkbox for the colors
             for (let i = 0; i < groupedColors.length; i++) {
+
+                // Initialize
                 const color = groupedColors[i];
+                
+                // Create the div.
                 const div = document.createElement("div");
                 div.classList.add("color-option");
 
@@ -49,11 +49,14 @@ document.addEventListener("DOMContentLoaded", () => {
                         ${color}
                     </label>
                 `;
-                colorsDiv.appendChild(div);
-
+                
                 // Fill the color box with the approriate color
                 const span = div.querySelector(".color-box");
                 span.style.backgroundColor = colorHexMap[color];
+
+                // Append it.
+                colorsDiv.appendChild(div);
+
             }
         })
         .catch(err => console.error("Error loading colors:", err));
