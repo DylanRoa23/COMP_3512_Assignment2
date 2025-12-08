44b5cc2f2003c4e7c7eff51478020b8f6da4eb76
Existenial <150177160+Existenial@users.noreply.github.com>
2025-12-08 15:48:28 -0700

    Moved nav listener behaviour to header.

--- droa858/js/about.js
+++ droa858/js/about.js
@@ -4,16 +4,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const aboutBtn = document.querySelector(".aboutBtn");
     const about = document.querySelector("#about");
 
-    // Open dialog box when clicking the About button
-    aboutBtn.addEventListener("click", (event) => {
-        event.preventDefault();   // Prevent the <a> tag from reloading the page
-        about.showModal();
-    });
-
     // Close dialog when clicking the close button
     about.addEventListener("click", (e) => {
         if (e.target.id === "closeAbout") {
             about.close();
         }
     });
+    
 });
