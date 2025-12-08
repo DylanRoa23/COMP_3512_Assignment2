60cd63eff17d524b9cfb0a644d5d45eff760149c
Existenial <150177160+Existenial@users.noreply.github.com>
2025-12-08 15:48:46 -0700

    Removed useless variable.

--- droa858/js/about.js
+++ droa858/js/about.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     // Configurable variables.
-    const aboutBtn = document.querySelector(".aboutBtn");
     const about = document.querySelector("#about");
 
     // Close dialog when clicking the close button
